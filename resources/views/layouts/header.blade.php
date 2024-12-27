be73115d1649d9d5a7e264da2813ec34bd6b20f4
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-12-27 15:36:57 +0100

    Header skeleton done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <div class="h-full flex justify-around text-black dark:text-white">
-    <div class="w-4/5 content-center">
-        <h1 class="text-3xl">FestivalFlex</h1>
-        <i class="fa-regular fa-user"></i>
+    <div class="w-4/5 items-center flex justify-between">
+        <h1 class="text-3xl"><a href="{{route('home')}}">FestivalFlex</a></h1>
+        <a href="{{route('profile')}}"><i class="fa-regular fa-user fa-2xl"></i></a>
     </div>
 </div>
