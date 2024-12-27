e92956215ec2c9f756f0e0a2b4ea6b2eeb3f5d1d
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-12-27 15:19:30 +0100

    Added font awesome while working on header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,6 @@
-<div>
-    <div>
-        <h1>FestivalFlex</h1>
-    </div>
-    <div>
-
+<div class="h-full flex justify-around text-black dark:text-white">
+    <div class="w-4/5 content-center">
+        <h1 class="text-3xl">FestivalFlex</h1>
+        <i class="fa-regular fa-user"></i>
     </div>
 </div>
