722e28fbc988db00e3cbd398de6b0371bfdec590
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-01-01 16:47:06 +0100

    Home placeholder image, and starting on trip maker

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,8 +1,44 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="h-[200vh] dark:text-white text-black">
-    <p>Home sweet home</p>
+<div class="h-[200vh] dark:text-white text-black flex flex-col overflow-hidden px-[10vw]">
+    <div class="h-[40vh] w-screen -mx-[10vw]">
+        <img src="/1440x300.svg" alt="">
+    </div>
+    <div class="h-[20vh] -mb-24">
+        <!-- trip menu -->
+        <div class="bg-slate-600 w-full h-4/6 -translate-y-2/3">
+            <form>
+                <div> <!-- from -->
+                    <label>From</label>
+                    <input>
+                </div>
+                <div> <!-- to -->
+                    <label>To</label>
+                    <input>
+                </div>
+                <div> <!-- departure -->
+                    
+                </div>
+                <div> <!-- passengers -->
+    
+                </div>
+                <div> <!-- submit -->
+    
+                </div>
+            </form>
+        </div>
+    </div>
+    <x-linebreak/>
+    <div class="h-[50vh]">
+        <!-- upcoming festivals -->
+        s
+    </div>
+    <x-linebreak/>
+    <div class="h-[50vh]">
+        <!-- festival news -->
+        d
+    </div>
 </div>
 
 @endsection
