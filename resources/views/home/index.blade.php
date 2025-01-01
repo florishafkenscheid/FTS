dd5b2ac8828c10e954e8cdd701916f0e9ad74e44
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-01-01 17:16:44 +0100

    Trip maker complete

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,23 +8,26 @@
     <div class="h-[20vh] -mb-24">
         <!-- trip menu -->
         <div class="bg-slate-600 w-full h-4/6 -translate-y-2/3">
-            <form>
-                <div> <!-- from -->
-                    <label>From</label>
-                    <input>
+            <form class="flex h-full px-6 items-center justify-around">
+                <div class="flex flex-col -mr-8 w-1/4"> <!-- from -->
+                    <label class="px-3">From</label>
+                    <input class="bg-slate-400 rounded-md w-11/12">
                 </div>
-                <div> <!-- to -->
-                    <label>To</label>
-                    <input>
+                <div class="flex flex-col w-1/4"> <!-- to -->
+                    <label class="px-3">To</label>
+                    <input class="bg-slate-400 rounded-md w-11/12">
                 </div>
-                <div> <!-- departure -->
-                    
+                <div class="flex flex-col ml-12 -mr-8 w-1/5"> <!-- departure -->
+                    <label class="px-3">Departure</label>
+                    <input class="bg-slate-400 rounded-md w-11/12" type="date">
                 </div>
-                <div> <!-- passengers -->
-    
+                <div class="flex flex-col w-1/5"> <!-- passengers -->
+                    <label class="px-3">Passengers</label>
+                    <input class="bg-slate-400 rounded-md w-11/12" type="number" value="0">
                 </div>
-                <div> <!-- submit -->
-    
+                <div class="flex size-12 justify-center items-center"> <!-- submit -->
+                    <i class="fa-solid fa-arrow-up fa-xl absolute -z-10"></i>
+                    <input type="submit" value class="size-12 cursor-pointer">
                 </div>
             </form>
         </div>
