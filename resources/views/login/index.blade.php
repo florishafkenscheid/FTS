43ac6b1561e3f7d4e4e09a102c58118d301b0656
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-01-04 00:08:52 +0100

    Login page done

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+@extends('layouts.app')
+@section('content')
+
+<div class="h-[85vh] w-full overflow-hidden flex justify-center items-center">
+    <div class="bg-slate-600 h-5/6 w-2/3 rounded-md flex flex-row">
+        <div class="w-1/2 h-full flex items-center justify-end">
+            <div class="bg-white h-[90%] w-[95%] rounded-lg">
+            </div>
+        </div>
+        <div class="w-1/2 h-full flex items-center justify-center">
+            <div class="w-full px-8 py-16 h-full flex flex-col gap-20">
+                <div class="flex flex-col">
+                    <h2 class="text-4xl">Log in</h2>
+                    <h4>Don't have an account? <a href="/login" class="text-blue-400">Register</a></h4>
+                </div>
+                <form class="flex flex-col gap-8">
+                    <div class="flex flex-col gap-2">
+                        <input id="email" type="email" placeholder="Email"
+                        class="rounded-lg">
+                        <input id="password" type="password" placeholder="Enter your password"
+                        class="rounded-lg">
+                    </div>
+                    <input id="submit" type="submit" value="Log in"
+                    class="h-fit w-full rounded-lg bg-slate-600 cursor-pointer">
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
+@endsection
