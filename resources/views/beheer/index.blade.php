ae6dfc11113f92712b72523e8dd18303b4702043
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-01-07 08:51:24 +0100

    Finishing touches of beheer pagina

--- resources/views/beheer/index.blade.php
+++ resources/views/beheer/index.blade.php
@@ -3,8 +3,11 @@
 
 <div class="h-[85vh] overflow-hidden flex flex-row">
     <div class="h-full w-1/3">
-        <div class="h-1/6 w-full flex justify-center items-center">
+        <div class="h-1/6 w-full flex flex-row justify-center items-center gap-4">
             <h2 class="text-white text-3xl">Upcoming</h2>
+            <a href="{{route('create_festival')}}">
+                <i class="fa-solid fa-plus fa-xl translate-y-1/4"></i>
+            </a>
         </div>
         <div class="h-5/6 w-full flex flex-col gap-4 items-center">
             <!-- logic needed -->
