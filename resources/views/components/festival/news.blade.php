d0cfed16f1a184312a549cc1bacd64e8b703a647
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-01-10 15:12:23 +0100

    Added a collapsible effect to festival posts

--- resources/views/components/festival/news.blade.php
+++ resources/views/components/festival/news.blade.php
@@ -1,11 +1,11 @@
-<div class="h-40 w-72 bg-slate-600 relative">
-    <div class="h-1/4 absolute">
+<div class="h-fit w-72 bg-slate-600 relative" id="container">
+    <div class="h-1/4">
         <img src="/288x40.svg"/>
     </div>
-    <div class="p-2 h-1/4 absolute top-1/4">
-        <h2 class="text-lg font-medium">{{$festivalNews->title}}</h2>
+    <div class="p-2 h-8">
+        <h2 class="absolute text-lg font-medium">{{$festivalNews->title}}</h2>
     </div>
-    <div class="h-1/2 overflow-hidden absolute top-1/2">
-        <p class="p-2 line-clamp-3 text-ellipsis">{{$festivalNews->content}}</p>
+    <div class="overflow-hidden cursor-pointer transition-all duration-300" id="collapsible">
+        <p class="p-2 -pb-2 text-ellipsis line-clamp-3 leading-7">{{$festivalNews->content}}</p>
     </div>
 </div>
